9878e8f4ee2b682c1767d7bfb5c6dfea9bf805b2
Jacek Berbecki <berbecki@gmail.com>
2019-02-10 12:31:21 +0100

    Task #7 add img generator and downloader

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import { DragDropContextProvider } from 'react-dnd'
 import HTML5Backend from 'react-dnd-html5-backend'
+import domtoimage from 'dom-to-image'
+import FileSaver from 'file-saver'
 
 import store from './store'
 
@@ -17,6 +19,16 @@ import './styles/variables.css'
 import styles from './styles/app.css'
 
 class App extends Component {
+    _saveToImg = () => {
+        const node = document.getElementById('dom_to_img')
+        domtoimage.toBlob(node)
+            .then(function (blob) {
+                FileSaver.saveAs(blob, 'my-project.png')
+            })
+            .catch(function (error) {
+                console.error('oops, something went wrong!', error);
+            });
+    }
     render() {
         return (
             <Provider store={store}>
@@ -26,11 +38,11 @@ class App extends Component {
                         <div className={styles.editorHeader}>
                             <h1>Simple Editor</h1>
                         </div>
-                        <div className={styles.editor}>
+                        <div id='dom_to_img' className={styles.editor}>
                             <EditorScene />
                         </div>
                         <div className={styles.editorAction}>
-                            <Button type={'action'}>download as image</Button>
+                            <Button type={'action'} onClickCallback={this._saveToImg}>download as image</Button>
                         </div>
                         <div className={styles.addLogo}>
                             <h2>Add logo</h2>
